abd002aa6f634014d0f522156366c1c18c8e6e5b
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-06-22 12:34:52 -0500

    taller_1

--- unidad_01/javascript/basic/01_taller/js/script.js
+++ unidad_01/javascript/basic/01_taller/js/script.js
@@ -1,3 +1,13 @@
+var contador = 1
+
 function cargar_elemento(){
-    var contaienr = document.getElementById("container");
+    var content = document.getElementById("content")
+
+    var div = document.createElement("div")
+    div.className = "item-ox"
+    var texto = document.createTextNode("Item " + contador)
+    contador++
+    div.appendChild(texto)
+
+    content.appendChild(div)
 }
