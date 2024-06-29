c3f9c5ef4fc541cc90de840417d36705c6e9b929
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-06-28 21:14:35 -0500

    08_Taller

--- unidad_01/javascript/basic/07_taller/js/script.js
+++ unidad_01/javascript/basic/07_taller/js/script.js
@@ -1,28 +1,21 @@
 var div_cajita = document.getElementById("cajita")
-var es_presionado = false
-var offset = [0,0]
+var  id = null
+var pos = 0
 
-div_cajita.addEventListener("mousedown", function(valor){
-    es_presionado = true
+function mover(){
+    clearInterval(id)
+    id = setInterval(frame, 5)
+    // El setInterval es para que se repita varias veces
 
-    offset = [ 
-        div_cajita.offsetLeft - valor.clientX,
-        div_cajita.offsetTop - valor.clientY
-    ]
+}
 
-}, true)
-
-div_cajita.addEventListener("mouseup", function(valor) {
-    es_presionado = false
-}, true)
-
-div_cajita.addEventListener("mousemove", function(valor) {
-    valor.preventDefault()
-
-    if (es_presionado){
-        div_cajita.style.left = (valor.clientX + offset[0]) + "px"
-        div_cajita.style.top = (valor.clientY + offset[1]) + "px"
+function frame(){
+    if (pos == 350) {
+        clearInterval(id)
+    }else{
+        pos++
+        div_cajita.style.top = pos + "px"  /* si solo queda este codigo la bolita se moveria a la derecha */
+        div_cajita.style.left = pos + "px"  /* si solo queda este codigo la bolita se moveria hacia abajo */
     }
-}, true)
 
-    
+}
