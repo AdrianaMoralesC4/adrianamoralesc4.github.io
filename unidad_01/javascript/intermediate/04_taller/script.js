d24cd8c836c8d13035284666bbfd2e2a3c3a04bf
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-06-29 12:10:12 -0500

    04_taller

---
+++ unidad_01/javascript/intermediate/04_taller/script.js
@@ -0,0 +1,26 @@
+// Tipos de funciones
+
+//funciones declarativa
+function imprimir(){
+    console.log('Hola Mundo')
+}
+
+//Fucion expresiva o tambien conocidad como anonima
+const suma = function(a,b){
+    return(a + b)
+}
+
+console.log(suma(10, 20))
+
+//Funcion flecha
+const suma2 = (a, b) => {
+    let total = a + b
+    console.log(`La suma de ${a} y ${b} es ${total}`)
+}
+
+suma2(100,200)
+
+//se puede realizarllo en una sola linea
+const suma3 = (a, b) => a + b
+
+console.log(suma(110, 220))
