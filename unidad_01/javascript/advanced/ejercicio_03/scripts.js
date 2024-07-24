d99a640202d15a9249b48d68e7ac08e04b6e0add
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-07-23 20:43:31 -0500

    index.js

--- unidad_01/javascript/advanced/ejercicio_03/scripts.js
+++ unidad_01/javascript/advanced/ejercicio_03/scripts.js
@@ -5,26 +5,37 @@
 const URL = 'https://rickandmortyapi.com/api/character/'
 var xhttp = null
 
-function conectar(personaje){
-    xhttp = new XMLHttpRequest() 
+function obtener_personajes() {
+    for (let i=1; i<=10; i++) {
+        conectar(i)
+    }
+}
+
+function conectar(personaje) {
+    xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = obtener_respuesta
     xhttp.open('GET', URL + personaje, true)
     xhttp.send()
 }
 
-function obtener_respuesta(){
+var personaje = ''
+
+function obtener_respuesta() {
     if (xhttp != null && xhttp.readyState == 4) {
         if (xhttp.status == 200) {
-            let dato = JSON.parse(xhttp.responseText)
-            // console.log(dato)
+            let dato =  JSON.parse(xhttp.responseText)
+            console.log(dato)
 
             let container = document.getElementById('container')
-            // let h1 = document.createElement()
-            // let personaje =  `<h1>${dato.name}</h1>
-            // <img src="${dato.image}" />`
-            let personaje =  `<div class="container-item"><h1>${dato.name}</h1>
-            <img src="${dato.image}" /><div class="container-item">`
-            container.innerHTML = personaje
+            let container_item = document.createElement('div')
+            let img = document.createElement('img')
+            img.src = dato.image
+            let h1 = document.createElement('h1')
+            let name = document.createTextNode( dato.name )
+            h1.appendChild(name)
+            container_item.appendChild(img)
+            container_item.appendChild(h1)
+            container.appendChild(container_item)
         }
     }
 }
