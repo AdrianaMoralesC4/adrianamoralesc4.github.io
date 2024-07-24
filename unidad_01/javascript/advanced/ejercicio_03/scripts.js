133b553a3d62a2cad11492ce5812eb8f6d290904
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-07-23 20:17:21 -0500

    ejericio_03

---
+++ unidad_01/javascript/advanced/ejercicio_03/scripts.js
@@ -0,0 +1,30 @@
+// FUNCIONES TIPO CALLBACKS
+// CONEXION ENTRE EL CLIENTE Y EL SERVIDOR
+// AJAX = Asynchronous Javascript and XML
+
+const URL = 'https://rickandmortyapi.com/api/character/'
+var xhttp = null
+
+function conectar(personaje){
+    xhttp = new XMLHttpRequest() 
+    xhttp.onreadystatechange = obtener_respuesta
+    xhttp.open('GET', URL + personaje, true)
+    xhttp.send()
+}
+
+function obtener_respuesta(){
+    if (xhttp != null && xhttp.readyState == 4) {
+        if (xhttp.status == 200) {
+            let dato = JSON.parse(xhttp.responseText)
+            // console.log(dato)
+
+            let container = document.getElementById('container')
+            // let h1 = document.createElement()
+            // let personaje =  `<h1>${dato.name}</h1>
+            // <img src="${dato.image}" />`
+            let personaje =  `<div class="container-item"><h1>${dato.name}</h1>
+            <img src="${dato.image}" /><div class="container-item">`
+            container.innerHTML = personaje
+        }
+    }
+}
