b532c760cd515416393cc584724b76d9979fdeab
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-08-23 21:12:42 -0500

    taller5

--- unidad_04/taller_05/server.js
+++ unidad_04/taller_05/server.js
@@ -1,11 +1,19 @@
-const express = require('express') //Se necesita el paquete express
+const express = require('express')
+const body_parser = require('body-parser')
+
+const config = require('./config')
+const routes = require('./network/routes')
+
+const db = require('./db')
 
 var app = express()
 
-app.use('/', function(req, res){
-    res.send('HOLA MUNDOO!!!')
-})
+db( config.DB_URL )
 
-let puerto = 3000
-app.listen(puerto) // app.listen(puerto)
-console.log(`La aplicacion se encuentra funcionando en http://localhost:${puerto}/`)
+app.use( body_parser.json() )
+app.use( body_parser.urlencoded({extended:false}) )
+
+routes( app )
+
+app.listen( config.PORT )
+console.log(`La aplicacion se encuentra arriba en http://localhost:${config.PORT}`)
