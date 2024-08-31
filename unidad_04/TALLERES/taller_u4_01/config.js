63466fdcf5e5cf9a2007223d45dce29978f83941
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-08-31 00:54:15 -0500

    talleru401

---
+++ unidad_04/TALLERES/taller_u4_01/config.js
@@ -0,0 +1,7 @@
+const config = {
+    PORT: 3000,
+    //DB_URL: 'mongodb://gpizarro:12345@localhost:27017/miapp?authSource=admin'
+    DB_URL: 'mongodb+srv://amorales:amoralesc4@cluster0.wgplk.mongodb.net/'
+}
+
+module.exports = config
--- unidad_04/TALLERES/taller_u4_01/network/routes.js
+++ unidad_04/TALLERES/taller_u4_01/network/routes.js
@@ -1,12 +1,8 @@
 const usuario = require('../components/usuario/interface')
-// const ciudad = require('../components/ciudad/interface')
-// const pais = require('../components/pais/interface')
 
 const routes = function( server ) {
     server.use('/', usuario)
     // server.use('/usuario', usuario)
-    // server.use('/ciudad', ciudad)
-    // server.use('/pais', pais)
 }
 
 module.exports = routes
