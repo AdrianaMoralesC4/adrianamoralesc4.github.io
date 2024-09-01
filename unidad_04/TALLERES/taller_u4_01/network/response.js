a8423b1f03a6f18a30c857f863ef8edaaf75a62a
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-08-31 22:04:04 -0500

    taller_u401

--- unidad_04/TALLERES/taller_u4_01/network/response.js
+++ unidad_04/TALLERES/taller_u4_01/network/response.js
@@ -8,8 +8,12 @@ const path = require('path');
 // }
 
 exports.success = function(req, res, data, num_status) {
-    res.status(num_status).sendFile(path.join(__dirname, '../views', 'index_user.html'));
+    res.status( num_status ).send( { error:'', body:data })
 }
+
+// exports.success = function(req, res, data, num_status) {
+//     res.status(num_status).sendFile(path.join(__dirname, '../views', 'index_user.html'));
+// }
 
 exports.error = function(req, res, message, num_status) {
     res.status( num_status ).send( { error:message, body:'' } )
--- unidad_04/taller_04/network/response.js
+++ unidad_04/taller_04/network/response.js
@@ -1,5 +1,5 @@
 exports.success = function(req, res, data, num_status) {
-    res.status( num_status ).send( { error:'ESTAS EN USUARIO', body:data })
+    res.status( num_status ).send( { error:'', body:data })
 }
 
 exports.error = function(req, res, message, num_status) {
