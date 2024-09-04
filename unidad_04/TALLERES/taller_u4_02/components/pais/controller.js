22cf51d36abe2be890a6fdcb36beb8d287c0496c
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-09-04 13:20:34 -0500

    TALLER_U4_02

---
+++ unidad_04/TALLERES/taller_u4_02/components/pais/controller.js
@@ -0,0 +1,37 @@
+const storage = require('./storage')
+
+function insertar_pais( dato ) {
+    return new Promise( (resolve, reject) => {
+        if (!dato.nombre ) {
+            reject( 'Los datos se encuentran incompletos.' )
+        } else {
+            resolve( storage.insertar( dato ) )
+        }
+    } )
+}
+
+function obtener_pais( dato ) {
+    return new Promise( (resolve, reject) => {
+        if (!dato) {
+            reject( 'No existen datos' )
+        } else {
+            resolve( storage.obtener( dato ) )
+        }
+    } )
+}
+
+function eliminar_pais(dato) {
+    return new Promise( (resolve, reject) => {
+        if ( !dato.nombre ) {
+            reject( 'Los datos se encuentran incompletos.' )
+        } else {
+            resolve( storage.eliminar( dato ) )
+        }
+    } ) 
+}
+
+module.exports = {
+    insertar_pais,
+    obtener_pais,
+    eliminar_pais
+}
