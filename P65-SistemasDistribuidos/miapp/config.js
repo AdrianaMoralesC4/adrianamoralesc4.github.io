f74ea72e60aa0df75c4d215fbe261309dcc39381
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-11-28 23:57:46 -0500

    SD_PRACTICA

---
+++ P65-SistemasDistribuidos/miapp/config.js
@@ -0,0 +1,7 @@
+const config = {
+    PORT: 3000,
+    DB_URL: 'mongodb://dba:12345@mongodb:27017/miapp?authSource=admin'
+    //DB_URL: 'mongodb+srv://amorales:amoralesc4@cluster0.wgplk.mongodb.net/HOLAA?retryWrites=true&w=majority'
+}
+
+module.exports = config
