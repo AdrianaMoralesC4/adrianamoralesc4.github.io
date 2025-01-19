779dc0d2132fd829bf8a1fd0bfaf2f0a156049af
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2025-01-19 13:30:28 -0500

    microservicio_ejemplo

--- P65-SistemasDistribuidos/microservicio_ejemplo/app/index.js
+++ P65-SistemasDistribuidos/microservicio_ejemplo/app/index.js
@@ -13,4 +13,8 @@ res.send(`appid: ${appid} app2 page: says hello!`))
 app.get("/admin", (req,res) => 
 res.send(`appid: ${appid} ADMIN page: very few people should see this`))
 
-app.listen(appid, ()=>console.log(`${appid} is listening on ${appid}`))
+//app.listen(appid, ()=>console.log(`${appid} is listening on ${appid}`))
+
+app.listen(8080, () => {
+    console.log("Server running on port 8080");
+});  
